5f10f92ab9a24238bf2cfde2aa2bcebf9459bc1b
Matthew Ng <ngm1@lg12-19.scss.tcd.ie>
2019-02-18 13:57:41 +0000

    added provided code

---
+++ astar.pl
@@ -0,0 +1,64 @@
+:- dynamic( kb/1 ).
+
+makeKB( File ) :-
+    open( File, read, Str ),
+    readK( Str, K ), 
+    reformat( K, KB ), 
+    asserta( kb( KB ) ), 
+    close( Str ).
+
+readK( Stream, [] ) :- 
+    at_end_of_stream( Stream ),
+    !.
+
+readK( Stream, [X | L] ) :- 
+    read( Stream, X ),
+    readK( Stream, L ).
+
+reformat( [], [] ).
+
+reformat( [end_of_file], [] ) :- 
+    !.
+
+reformat( [:-(H, B) | L], [[H | BL] | R] ) :- 
+    !,
+    mkList( B, BL ),
+    reformat( L, R ).
+
+reformat( [A | L], [[A] | R] ) :- 
+    reformat(L,R).
+
+mkList( (X, T), [X | R] ) :- 
+    !,
+    mkList( T, R ).
+
+mkList( X, [X] ).
+
+initKB( File ) :- 
+    retractall( kb(_) ), 
+    makeKB( File ).
+
+astar( Node, Path, Cost ) :- 
+    kb( KB ),
+    astar( Node, Path, Cost, KB ).
+
+% astar(Node, Path, Cost, KB) :- ???
+
+arc( [H | T], Node, Cost, KB ) :-
+    member( [H | B], KB ),
+    append( B, T, Node ),
+    length( B, L ),
+    Cost is L+1.
+
+heuristic( Node, H ) :-
+    length( Node, H ).
+
+goal( [] ).
+
+less-than( [[Node1 | _], Cost1], [[Node2 | _], Cost2] ) :-
+    heuristic( Node1, Hvalue1 ),
+    heuristic( Node2, Hvalue2 ),
+    F1 is Cost1 + Hvalue1, 
+    F2 is Cost2 + Hvalue2,
+    F1 =< F2.
+
