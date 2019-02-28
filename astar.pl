1937545da728f91cfade6f6b1ff1a5c11410f318
Matthew Ng <ngm1@tcd.ie>
2019-02-28 21:58:22 +0000

    Finished astar

--- astar.pl
+++ astar.pl
@@ -45,18 +45,19 @@ astar( Node, Path, Cost ) :-
     kb( KB ),
     astar( [[Node, [Node], 0]], Path, Cost, KB ).
 
-arc( [H | T], Dest, Cost, KB ) :-
+arc( [[H | T], OPath, OCost], [Dest, [Dest | OPath], Cost], KB ) :-
     member( [H | B], KB ),
     append( B, T, Dest ),
     length( B, L ),
-    Cost is L+1.
+    Tmp is L+1,
+    Cost is OCost + Tmp.
 
 heuristic( Node, H ) :-
     length( Node, H ).
 
 goal( [] ).
 
-less-than( [[Node1 | _], Cost1], [[Node2 | _], Cost2] ) :-
+less-than( [Node1, _, Cost1], [Node2, _, Cost2] ) :-
     heuristic( Node1, Hvalue1 ),
     heuristic( Node2, Hvalue2 ),
     F1 is Cost1 + Hvalue1,
@@ -64,14 +65,29 @@ less-than( [[Node1 | _], Cost1], [[Node2 | _], Cost2] ) :-
     F1 =< F2.
 
 astar( [[Node, Path, Cost] | _], Path, Cost, _ ) :- 
-    goal( Node ).
+    goal( Node ),
+    !.
 
-astar( [[Node, Path, Cost] | Rest], Path, Cost, KB ) :-
-    findall( X, arc( Node, X ), Children ),
-    add-to-frontier( Children, [Node, Path, Cost] | Rest], NewFrontier ),
-    astar( NewFrontier, [Node | Path], Cost, KB).
+astar( [Head | Rest], FinalPath, FinalCost, KB ) :-
+    findall( Child, arc( Head, Child, KB ), Children ),
+    add_to_frontier( Children, Rest, NewFrontier ),
+    astar( NewFrontier, FinalPath, FinalCost, KB).
 
-add-to-frontier( [C | RestChild], [HeadFront | RestOldFrontier], NewFrontier ) :-
-    less-than(  )
-    add-to-frontier(  ).
+add_to_frontier( [], Front, Front ).
+
+add_to_frontier( [Child | MoreChildren], OldFront, NewFront ) :-
+    insert( Child, OldFront, PartialFront ),
+    add_to_frontier( MoreChildren, PartialFront, NewFront ).
+
+insert( Node, [], [Node] ).
+
+insert( Node, [Best | Rest], NewFront ) :-
+    less-than(Node, Best),
+    append( [Node], [Best | Rest], NewFront ),
+    !.
+
+insert( Node, [Best | Rest], NewFront ) :-
+    insert( Node, Rest, PartialFront ),
+    append( [Best], PartialFront, NewFront ).
+
 
